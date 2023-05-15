a6a665d1f96ea7916764d70972c0474dcd6500b7
Yasar Cakir <yasar.cakir@mirsis.com.tr>
2023-05-15 17:27:28 +0300

    Create footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.scss';
 import { Inter } from 'next/font/google';
 import Header from '@/components/layouts/Header';
+import Footer from '@/components/layouts/Footer';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -9,16 +10,13 @@ export const metadata = {
   description: 'Airbnb clone built with Next.js and Tailwind CSS.',
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <body className={inter.className}>
         <Header />
         <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
